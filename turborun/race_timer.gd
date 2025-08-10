6606c65f6299a86c4bb8b7833d5d75abfb36c541
Connor Jarvis <conjar93@gmail.com>
2025-08-10 16:12:55 -0700

    working speed and timer in a hud

--- turborun/race_timer.gd
+++ turborun/race_timer.gd
@@ -6,12 +6,16 @@ extends Label
 var elapsed: float = 0.0
 var running: bool = false
 
+func _ready() -> void:
+	# run + draw at least once
+	set_process(true)
+	queue_redraw()
+	# stick to bottom-left of the viewport (16 px padding)
+	set_anchors_preset(Control.PRESET_BOTTOM_LEFT)
+	add_theme_font_size_override("font_size", 26)  # 32 px font
+	position = Vector2(510, 20)   # x: from left, y: from bottom (negative goes up)
+
 func _process(delta: float) -> void:
-        if road_renderer:
-                if not running and road_renderer.current_speed > 0.1:
-                        running = true
-                if running:
-                        elapsed += delta
-        var minutes = int(elapsed) / 60
-        var seconds = int(elapsed) % 60
-        text = "%02d:%02d" % [minutes, seconds]
+	elapsed += delta
+	if road_renderer:
+		text = "Time: %.1f" %  elapsed  
